1f569adff2d7bca50507d9d822a8a14043e50749
elvece <lvcifferello@gmail.com>
2015-09-14 11:16:20 -0600

    added testing environment to clear db

--- test/test-server.js
+++ test/test-server.js
@@ -1,21 +1,69 @@
+process.env.NODE_ENV = 'test';
+
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var mongoose = require("mongoose");
+
 var server = require('../server/app');
+var Blob = require("../server/models/blob");
+
 var should = chai.should();
 
 chai.use(chaiHttp);
 
+//testing
 describe('Blobs', function() {
+
+  Blob.collection.drop();
+
+  beforeEach(function(done){
+    var newBlob = new Blob({
+      name: 'Bat',
+      lastName: 'man'
+    });
+    newBlob.save(function(err) {
+      done();
+    });
+  });
+  afterEach(function(done){
+    Blob.collection.drop();
+    done();
+  });
+
+
   it('should list ALL blobs on /blobs GET', function(done) {
     chai.request(server)
       .get('/blobs')
       .end(function(err, res){
         res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
         done();
       });
   });
+
   it('should list a SINGLE blob on /blob/<id> GET');
-  it('should add a SINGLE blob on /blobs POST');
+
+  it('should add a SINGLE blob on /blobs POST', function(done) {
+    chai.request(server)
+      .post('/blobs')
+      .send({'name': 'Java', 'lastName': 'Script'})
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('SUCCESS');
+        res.body.SUCCESS.should.be.a('object');
+        res.body.SUCCESS.should.have.property('name');
+        res.body.SUCCESS.should.have.property('lastName');
+        res.body.SUCCESS.should.have.property('_id');
+        res.body.SUCCESS.name.should.equal('Java');
+        res.body.SUCCESS.lastName.should.equal('Script');
+        done();
+      });
+  });
+
   it('should update a SINGLE blob on /blob/<id> PUT');
+
   it('should delete a SINGLE blob on /blob/<id> DELETE');
 });
